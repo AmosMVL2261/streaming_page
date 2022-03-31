b448442e32894ec9abc4e7da83a66a7bba5b110f
Amós Vega <refi2261@gmail.com>
2022-03-31 16:50:19 -0600

    Almost finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 function push_left(){
     document.getElementById("main").style.margin = "0px 0px 0px 0px";
+    document.getElementById("btn").style.left = "1vw";/*El boton queda en su posicion inicial*/
+    
 }
 
 function push_right(){
     document.getElementById("main").style.margin = "0px 0px 0px 250px";
+    document.getElementById("btn").style.left = "-10vw"; /*El boton queda oculto cuando se recorre verticalmente la pagina*/
 }
 
 document.getElementById("cancel").addEventListener("click", push_left);
